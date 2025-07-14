9c76daa003b17be0ca0672b394fa729a5e556e3d
Josh-Aguiluz <josh.dizon.aguiluz25@gmail.com>
2025-07-14 11:11:29 +0800

    Delete aboutModule.js

--- aboutModule.js
+++
@@ -1,8 +0,0 @@
-// Function to generate the about page message using the provided name
-function getAboutMessage(name) {
-    // Uses the passed 'name' parameter
-    return `Hello ${name}. This activity will teach on how to deal with a simple server and local modules in Node.js`;
-}
-
-// Export the function
-module.exports.generateAbout = getAboutMessage;

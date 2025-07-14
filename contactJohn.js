dc350bb6e256a717152f61108f47cb1ec31c7841
Josh-Aguiluz <josh.dizon.aguiluz25@gmail.com>
2025-07-14 11:11:17 +0800

    Delete contactJohn.js

--- contactJohn.js
+++
@@ -1,8 +0,0 @@
-// Function to generate the contact page message using the provided name
-function getContactMessage(name) {
-    // Uses the passed 'name' parameter
-    return `${name}, if you want additional details about this activity go to this site: <a href="https://www.tutorialsteacher.com/nodejs/nodejs-tutorials" target="_blank">https://www.tutorialsteacher.com/nodejs/nodejs-tutorials</a>`;
-}
-
-// Export the function
-module.exports.generateContact = getContactMessage;
